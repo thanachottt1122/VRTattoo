76fe619119e170591f7ec3de55151021295e1197
thanachottt1122 <thanachottt1122@gmail.com>
2026-08-26 17:40:25 +0700

    Setup
    Progress_1_Main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("TattooRoom");
+    }
+
+    public void OnSettingsButtonClicked()
+    {
+        Debug.Log("เปิดหน้าตั้งค่า");
+    }
+}
